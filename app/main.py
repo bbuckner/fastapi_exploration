2c73e393680bef7f8f45e1aaff8cb7f7e803b53f
bbuckner <brandon.buckner373@gmail.com>
2022-12-19 01:21:03 -0500

    CORS origins + middleware

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from . import models
 from .database import engine
@@ -8,6 +9,7 @@ from .routers import posts, users, auth, vote
 # uvicorn <path to file with app instance from current working directory>:<name of variable fastapi instance is stored in> --reload
 # example if fastapi_exploration is your current working directory:
 # uvicorn app.main:app --reload
+# The reload flag reloads on save.
 
 # This is what creates tables if they dont already exist.
 # Dont need this with alembic as it would make the
@@ -15,6 +17,14 @@ from .routers import posts, users, auth, vote
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(posts.router)
 app.include_router(users.router)
